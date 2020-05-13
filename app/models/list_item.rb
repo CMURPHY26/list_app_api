0455e1e3359a16a68f982701eedc84a78d15156f
Chris <28192055+CMURPHY26@users.noreply.github.com>
2020-05-12 22:00:42 -0400

    updated scope for list item ordering

--- app/models/list_item.rb
+++ app/models/list_item.rb
@@ -1,3 +1,4 @@
 class ListItem < ApplicationRecord
     belongs_to :list
+    default_scope { order(priority: :asc)}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :lists do 
-    resources :list_items, only: [:create]
+    resources :list_items, only: [:create, :show]
   end
   resources :list_items
 
